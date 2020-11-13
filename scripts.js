41ed70dedc5768d5cc7928dffd646fff0fc69be5
danielmedb <70508723+danielmedb@users.noreply.github.com>
2020-11-13 08:31:18 +0100

    updates

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+// SCROLL TO TOP
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+  
+
+// SCROLL FUNCTION
 const anchors = document.querySelector(".anchors");
 anchors.firstElementChild.firstElementChild.firstElementChild.classList.add('active');
 
@@ -19,7 +27,50 @@ const pageable = new Pageable("main", {
     },
 });
 
+// Menu click function
 
-const bgConceptKey = document.getElementById('conceptKey').style.background;
-// .style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0]
-console.log(bgConceptKey);
+anchors.addEventListener("click", (e) => {
+    e.preventDefault();
+    const anchor = e.target.getAttribute('href');
+    pageable.scrollToAnchor(`#${anchor}`);
+    
+});
+
+
+const nav = () => {
+    
+    const hamburger = document.querySelector(".hamburger-menu");
+    const mobileNav = document.querySelector(".mobile-nav");
+    const navLinks = document.querySelectorAll(".mobile-nav li");
+    const mobileNavActive = document.getElementsByClassName('.mobile-nav-active');
+    const logo = document.querySelector('.logo');
+
+
+
+    // HAMBURGER-MENU FUNCTION
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("toggle");
+        mobileNav.classList.toggle('mobile-nav-active');
+        console.log(mobileNavActive);
+        // if(mobileNavActive.length > 0){
+        //     console.log(logo); 
+        //     console.log('test');
+            
+        // }
+        
+
+        // Toggle mobile-link on/off
+        navLinks.forEach((link, index) => {
+            if(!link.style.animation){
+                link.style.animation = `navLinkFadeIn 1s ease forwards ${index / navLinks.length + 0.4}s`;
+            }else{
+                link.style.animation = '';
+            }
+        });
+
+    });
+
+
+};
+
+nav();
